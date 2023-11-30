b851140b4f777d08cf50e09da9afe2512123cebf
Leszek Mielnikow <63610219+LexiestLeszek@users.noreply.github.com>
2023-11-30 08:35:28 +0100

    Added ability to edit, reply to, delete, upvote, and downvote comments.

--- inComment.js
+++ inComment.js
@@ -1,71 +1,106 @@
 (function() {
-  let commentBoxes = [];
-  let commentIdCounter = 1;
+ let commentBoxes = [];
+ let commentIdCounter = 1;
 
-  function createCommentBox(x, y, parentId = null) {
-    const commentBox = document.createElement('div');
-    const commentId = `comment_${commentIdCounter}`;
-    commentBox.classList.add('comment-box');
-    commentBox.dataset.commentId = commentId;
-    commentBox.style.position = 'absolute';
-    commentBox.style.top = `${y}px`;
-    commentBox.style.left = `${x}px`;
-    commentBox.innerHTML = `
-      <textarea placeholder="Write your comment here"></textarea>
-      <button class="submitBtn">Submit</button>
-      <button class="cancelBtn">Cancel</button>
-    `;
-    document.body.appendChild(commentBox);
+ function createCommentBox(x, y, parentId = null) {
+   const commentBox = document.createElement('div');
+   const commentId = `comment_${commentIdCounter}`;
+   commentBox.classList.add('comment-box');
+   commentBox.dataset.commentId = commentId;
+   commentBox.style.position = 'absolute';
+   commentBox.style.top = `${y}px`;
+   commentBox.style.left = `${x}px`;
+   commentBox.innerHTML = `
+     <textarea placeholder="Write your comment here"></textarea>
+     <button class="submitBtn">Submit</button>
+     <button class="cancelBtn">Cancel</button>
+     <button class="editBtn">Edit</button>
+     <button class="replyBtn">Reply</button>
+     <button class="deleteBtn">Delete</button>
+     <button class="upvoteBtn">Upvote</button>
+     <button class="downvoteBtn">Downvote</button>
+     <div class="commentCount">Comments: 0</div>
+   `;
+   document.body.appendChild(commentBox);
 
-    const submitBtn = commentBox.querySelector('.submitBtn');
-    const cancelBtn = commentBox.querySelector('.cancelBtn');
+   const submitBtn = commentBox.querySelector('.submitBtn');
+   const cancelBtn = commentBox.querySelector('.cancelBtn');
+   const editBtn = commentBox.querySelector('.editBtn');
+   const replyBtn = commentBox.querySelector('.replyBtn');
+   const deleteBtn = commentBox.querySelector('.deleteBtn');
+   const upvoteBtn = commentBox.querySelector('.upvoteBtn');
+   const downvoteBtn = commentBox.querySelector('.downvoteBtn');
 
-    submitBtn.addEventListener('click', () => {
-      const commentText = commentBox.querySelector('textarea').value;
-      // Handle submission (e.g., send data to server or save locally)
-      console.log(`Comment ${commentId} submitted:`, commentText);
-      removeCommentBox(commentBox);
+   submitBtn.addEventListener('click', () => {
+     const commentText = commentBox.querySelector('textarea').value;
+     // Handle submission (e.g., send data to server or save locally)
+     console.log(`Comment ${commentId} submitted:`, commentText);
+     removeCommentBox(commentBox);
+     console.log('Parent ID:', parentId);
+   });
 
-      // For demonstration purposes, just logging the parentId
-      console.log('Parent ID:', parentId);
-    });
+   cancelBtn.addEventListener('click', () => {
+     removeCommentBox(commentBox);
+   });
 
-    cancelBtn.addEventListener('click', () => {
-      removeCommentBox(commentBox);
-    });
+   editBtn.addEventListener('click', () => {
+     const textarea = commentBox.querySelector('textarea');
+     textarea.value = textarea.value;
+     textarea.disabled = false;
+   });
 
-    commentBoxes.push({ id: commentId, box: commentBox });
-    commentIdCounter++;
+   replyBtn.addEventListener('click', () => {
+     createCommentBox(replyBtn.offsetLeft, replyBtn.offsetTop, commentBox.dataset.commentId);
+   });
 
-    return commentBox;
-  }
+   deleteBtn.addEventListener('click', () => {
+     const index = commentBoxes.findIndex(cb => cb.id === commentBox.dataset.commentId);
+     if (index > -1) {
+       commentBoxes.splice(index, 1);
+     }
+     removeCommentBox(commentBox);
+   });
 
-  function removeCommentBox(box) {
-    box.remove();
-    const index = commentBoxes.findIndex(cb => cb.box === box);
-    if (index > -1) {
-      commentBoxes.splice(index, 1);
-    }
-  }
+   upvoteBtn.addEventListener('click', () => {
+     // Increase the upvote count for the comment
+   });
 
-  document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
+   downvoteBtn.addEventListener('click', () => {
+     // Increase the downvote count for the comment
+   });
 
-    const existingBox = document.querySelector('.comment-box');
-    if (existingBox) {
-      removeCommentBox(existingBox);
-    }
+   commentBoxes.push({ id: commentId, box: commentBox });
+   commentIdCounter++;
 
-    const parentBox = event.target.closest('.comment-box');
-    if (parentBox) {
-      const parentBoxRect = parentBox.getBoundingClientRect();
-      const parentBoxX = parentBoxRect.left + parentBoxRect.width;
-      const parentBoxY = parentBoxRect.top;
-      createCommentBox(parentBoxX, parentBoxY, parentBox.dataset.commentId);
-    } else {
-      createCommentBox(mouseX, mouseY);
-    }
-  });
+   return commentBox;
+ }
+
+ function removeCommentBox(box) {
+   box.remove();
+   const index = commentBoxes.findIndex(cb => cb.box === box);
+   if (index > -1) {
+     commentBoxes.splice(index, 1);
+   }
+ }
+
+ document.addEventListener('contextmenu', function(event) {
+   event.preventDefault();
+   const mouseX = event.clientX;
+   const mouseY = event.clientY;
+
+   const existingBox = document.querySelector('.comment-box');
+   if (existingBox) {
+     removeCommentBox(existingBox);
+   }
+
+   const parentBox = event.target.closest('.comment-box');
+   if (parentBox) {
+     const parentBoxRect = parentBox.getBoundingClientRect();
+     const parentBoxX = parentBoxRect.left + parentBoxRect.width;
+     const parentBoxY = parentBoxRect.top;
+     createCommentBox(parentBoxX, parentBoxY, parentBox.dataset.commentId);
+   } else {
+     createCommentBox(mouseX, mouseY);
+   }
+ });
 })();
